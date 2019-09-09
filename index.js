17c68b42ac389dc0b0c7ed5bd449579260755e3d
lieby weintraub <liebahinda@gmail.com>
2019-09-09 17:24:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
  function BoardMember(name, homeState, training)
  this.name = name
-   this.homeState = homeState
+  this.homeState = homeState
   this.training = training
 
    this.prototype.veto = function(){
